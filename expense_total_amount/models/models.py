706d0026b11d968ddd9245c8152ce468931c8a28
Michelangelo Alcini <michelangelo.alcini@gesca.it>
2020-07-29 16:55:35 +0200

    fixed error of total amount calculation in expense_total_amount module

--- expense_total_amount/models/models.py
+++ expense_total_amount/models/models.py
@@ -7,10 +7,10 @@ from odoo.addons import decimal_precision as dp
 class Expense(models.Model):
     _inherit = 'hr.expense'
     
-    total_amount = fields.Monetary("Total", compute='_compute_total_amount', store=True, currency_field='currency_id', digits=dp.get_precision('Account'))
+    total_amount = fields.Monetary("Total", compute='_compute_amount', store=True, currency_field='currency_id', digits=dp.get_precision('Account'))
     
-    @api.depends('quantity', 'unit_amount', 'tax_ids', 'currency_id', 'x_studio_km_casa_lavoro','product_uom_id')
-    def _compute_total_amount(self):
+    @api.depends('date','quantity', 'unit_amount', 'tax_ids', 'currency_id', 'x_studio_km_casa_lavoro','product_uom_id')
+    def _compute_amount(self):
         for expense in self:
             if expense.product_uom_id.display_name == 'km':
                 expense.untaxed_amount = expense.unit_amount * (expense.quantity - expense.x_studio_km_casa_lavoro)
@@ -20,3 +20,6 @@ class Expense(models.Model):
                 expense.untaxed_amount = expense.unit_amount * expense.quantity
                 taxes = expense.tax_ids.compute_all(expense.unit_amount, expense.currency_id, expense.quantity, expense.product_id, expense.employee_id.user_id.partner_id)
                 expense.total_amount = taxes.get('total_included')
+                
+                
+        
